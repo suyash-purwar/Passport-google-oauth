633e1859bb3dca80879e20aa6b66d03936a51885
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-07-09 18:51:28 +0530

    Updated Google keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
    google: {
-      clientID: '262852765633-7ed26sgbll1hihr77cketbvecjd7a5i6.apps.googleusercontent.com',
-      clientSecret: '3wZ42iwiX_1_2_kh55dtKGs5'
+      clientID: '262852765633-k54k3mbv9klgnel0liickvis7098u3no.apps.googleusercontent.com',
+      clientSecret: 'ze35OSYgcyugf_AacHQbVH79'
    }
 }
