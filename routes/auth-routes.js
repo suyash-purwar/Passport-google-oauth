6713afb27b5ec7b931c76468b52d2caf90cb00f3
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-07-17 16:36:24 +0530

    Made profile view and added the functionality of logging out

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,13 +3,13 @@ const passport = require('passport');
 
 // auth login
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', {user: req.user});
 });
 
 // auth logout
 router.get('/logout', (req, res) => {
-    // handle with passport
-    res.send('logging out');
+    req.logout();
+    res.redirect('/');
 });
 
 // auth with google+
